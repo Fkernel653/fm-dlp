c14dde36481262085d48f3237a15abf6a7e3acd3
Fkernel653 <unkown11250@proton.me>
2026-05-17 16:41:55 +0500

    ✨️ Update version

--- fm-dlp.py
+++ fm-dlp.py
@@ -21,7 +21,7 @@ def main():
 
     fm_dlp = App(
         name="fm-dlp",
-        version="2.3.0",
+        version="2.3.2",
         help="fm-dlp is a CLI tool for searching YouTube/YTMusic and downloading audio/video from 1000+ platforms",
     )
 
