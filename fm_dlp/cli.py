be639ceb5ee4e6e7a6f22e9d972e2d33bed9f10f
Fkernel653 <unkown11250@proton.me>
2026-06-18 16:02:02 +0500

    Added build.yml for workflows

--- fm_dlp/cli.py
+++ fm_dlp/cli.py
@@ -51,7 +51,7 @@ def main():
     app = CLI(
         name="fm-dlp",
         description="CLI tool for searching YouTube/YTMusic and downloading audio/video from 1000+ platforms",
-        version="4.0.7",
+        version="4.0.8",
     )
 
     @app.command()
